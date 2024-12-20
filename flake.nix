b6ef9f0cd8a9601eb7c40e9b2302b0aef5cdbaf2
Thomas <mast3r@skade.dev>
2024-12-20 02:47:18 +0100

    added package

--- flake.nix
+++ flake.nix
@@ -16,5 +16,26 @@
             ];
             
         };
+        packages.${system} = rec {
+            source = gradle2nix.builders.x86_64-linux.buildGradlePackage rec {
+                name = "rp-utils";
+                version = "1.0";
+                pname = "wordcount";
+                lockFile = ./gradle.lock;
+                src = ./.;
+                gradleBuildFlags = ["build -x test"];
+                gradleInstallFlags = ["installDist -x test"];
+
+                installPhase = ''
+                    mkdir -p $out/{lib,share}/${name}
+                    cp ./app/build/libs/app-all.jar $out/lib/${name}/rp-utils.jar
+                    cp -r $src/* $out/share/${name}
+                '';
+            };
+            default = pkgs.writeScriptBin "rp-utils.sh" ''
+                #!${pkgs.bash}/bin/bash
+                ${pkgs.jdk21}/bin/java --enable-preview -jar ${source}/lib/rp-utils/rp-utils.jar $@
+            '';
+        };
     };
 }
